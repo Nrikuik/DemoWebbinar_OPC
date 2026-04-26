47a30f515e1ffbea2576f199d8ad77eb9a62293c
Eric Sanchez <ericsanchez.ingenieria@gmail.com>
2026-04-26 12:42:50 -0600

    ccms

---
+++ ProjectFiles/NetSolution/MainWindowBehavior.cs
@@ -0,0 +1,30 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.UI;
+using FTOptix.HMIProject;
+using FTOptix.NativeUI;
+using FTOptix.Retentivity;
+using FTOptix.CoreBase;
+using FTOptix.Core;
+using FTOptix.NetLogic;
+#endregion
+
+[CustomBehavior]
+public class MainWindowBehavior : BaseNetBehavior
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined behavior is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined behavior is stopped
+    }
+
+#region Auto-generated code, do not edit!
+    protected new MainWindow Node => (MainWindow)base.Node;
+#endregion
+}
